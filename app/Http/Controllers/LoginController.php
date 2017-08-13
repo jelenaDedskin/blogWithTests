9e0e2064c92a88089562133630254b3d3122b1b5
Goran Prijic <goran.prijic@gmail.com>
2017-08-13 20:16:05 +0200

    Exercise V - middlewares, basic usage

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'destroy']);
+    }
+
 	public function create()
 	{
 		return view('login.create');
@@ -17,7 +22,7 @@ class LoginController extends Controller
 		if (!auth()->attempt(
 				request(['email', 'password'])
 			)) {
-			
+
 			return back()->withErrors([
 				'message' => 'Bad credentials. Please try again'
 			]);
