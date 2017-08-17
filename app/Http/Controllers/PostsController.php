1147fe3c5df2389e8ae33c0e6cee395634cf6196
Mladen Kanostrevac <mladen.k@vivifyideas.com>
2017-08-17 14:03:17 +0200

    Exercise XIV - Eager loading users of posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,7 @@ class PostsController extends Controller
 
     public function index()
     {
-        $posts = Post::paginate(10);
+        $posts = Post::with('user')->paginate(10);
 
         return view('posts.index', ['posts' => $posts]);
     }
