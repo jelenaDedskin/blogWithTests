7c8b68e5d7ce967a5cce1c684fe61c7ddf2884ef
Mladen Kanostrevac <mladen.k@vivifyideas.com>
2017-08-17 15:27:27 +0200

    Exercise XVI - sidebar tags with view composer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -54,6 +54,20 @@
                 <h4>About</h4>
                 <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
             </div>
+
+            <div class="sidebar-module">
+                <h4>Tags</h4>
+                <ol class="list-unstyled">
+                    @foreach ($tags as $tag)
+                        <li>
+                            <a href="/posts/tags/{{ $tag->name }}">
+                                {{ $tag->name }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ol>
+            </div>
+
             <div class="sidebar-module">
                 <h4>Archives</h4>
                 <ol class="list-unstyled">
